60e880f1a2704c1b35c79a4769887db2c6e346b7
TimmyAlmmy <timalm282@gmail.com>
2026-04-02 12:01:29 +0100

    Fix temp folder for Render

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(fileUpload({ useTempFiles: true, tempFileDir: "/tmp/" }));
+app.use(fileUpload({ useTempFiles: true, tempFileDir: "./tmp" }));
 app.use(express.static(path.join(__dirname, "public")));
 
 // Cloudinary config
